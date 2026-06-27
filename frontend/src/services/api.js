731797e3b58c740ac338572a2802a6bdb370615f
taimoor897 <tsbhatti789@gmail.com>
2026-06-27 14:52:55 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((config) => {
 
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
